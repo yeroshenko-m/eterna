10eefb6c4d876cad6ab2088ed85bda9bac94ca0d
Mykhailo Yeroshenko <yeroshenko.m@icloud.com>
2022-12-07 23:10:04 +0200

    Initial Commit

---
+++ Eterna/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Eterna
+//
+//  Author:  Mykhailo Yeroshenko
+//  Created: 07.12.2022
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eterna/EternaApp.swift
@@ -0,0 +1,19 @@
+//
+//  EternaApp.swift
+//  Eterna
+//
+//  Author:  Mykhailo Yeroshenko
+//  Created: 07.12.2022
+//
+    
+
+import SwiftUI
+
+@main
+struct EternaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
